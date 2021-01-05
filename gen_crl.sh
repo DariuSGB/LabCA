51fd3248084c07ce3c935247c80946981822414a
Dario Garrido <55515143+DariuSGB@users.noreply.github.com>
2021-01-05 20:35:29 +0100

    Create gen_crl.sh

---
+++ gen_crl.sh
@@ -0,0 +1,20 @@
+#########################################################################
+# title: gen_crl.sh                                                     #
+# author: Dario Garrido                                                 #
+# date: 20210105                                                        #
+# description: Create CRL cert                                          #
+# usage: ./gen_crl.sh <ca_name>                                         #
+#########################################################################
+
+#!/bin/sh
+
+if [ "$#" -ne 1 ]
+then
+  echo "Usage: <ca_name>"
+  exit 1
+fi
+
+CA=$1
+FOLDER=.
+
+openssl ca -config $FOLDER/ca/$CA.cnf -gencrl -out $FOLDER/ca/$CA.crl
