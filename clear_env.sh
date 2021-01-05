469a1c09ab157c39fb402eefc90e89bf439a7aed
Dario Garrido <55515143+DariuSGB@users.noreply.github.com>
2021-01-05 20:56:46 +0100

    Create clear_env.sh

---
+++ clear_env.sh
@@ -0,0 +1,11 @@
+#########################################################################
+# title: clear_env.sh                                                   #
+# author: Dario Garrido                                                 #
+# date: 20210105                                                        #
+# description: Clear your enviroment                                    #
+# usage: ./clear_env.sh                                                 #
+#########################################################################
+
+#!/bin/sh
+
+rm -Rf ca/ certs/ db/ store/
