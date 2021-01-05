f0161eb8aa116553ce9b8a6eb2ac23b73cac880a
Dario Garrido <55515143+DariuSGB@users.noreply.github.com>
2021-01-05 20:31:00 +0100

    Create gen_cert.sh

---
+++ gen_cert.sh
@@ -0,0 +1,24 @@
+#########################################################################
+# title: gen_cert.sh                                                    #
+# author: Dario Garrido                                                 #
+# date: 20210105                                                        #
+# description: Create and sign a new certificate for a defined FQDN     #
+# usage: ./gen_cert.sh <ca_name> <fqdn>                                 #
+#########################################################################
+
+#!/bin/sh
+
+if [ "$#" -ne 2 ]
+then
+  echo "Usage: <ca_name> <fqdn>"
+  exit 1
+fi
+
+CA=$1
+DOMAIN=$2
+FOLDER=.
+
+sed -i -E "s/(^fqdn\s+=\s).*/\1$DOMAIN/" $FOLDER/ca/$CA.cnf
+
+openssl req -new -out $FOLDER/certs/$DOMAIN.csr -newkey rsa:2048 -keyout $FOLDER/certs/$DOMAIN.key -nodes -config $FOLDER/ca/$CA.cnf
+openssl ca -config $FOLDER/ca/$CA.cnf -in $FOLDER/certs/$DOMAIN.csr -out $FOLDER/certs/$DOMAIN.crt
