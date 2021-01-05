621cbbc472c783b21fe3e759d22d6eddb023bf14
Dario Garrido <55515143+DariuSGB@users.noreply.github.com>
2021-01-05 20:40:35 +0100

    Create ocsp_responder.sh

---
+++ ocsp_responder.sh
@@ -0,0 +1,19 @@
+#########################################################################
+# title: ocsp_responder.sh                                              #
+# author: Dario Garrido                                                 #
+# date: 20210105                                                        #
+# description: Run an OCSP responder on port 8080                       #
+# usage: ./ocsp_responder.sh <ca_name>                                  #
+#########################################################################
+
+#!/bin/sh
+
+if [ "$#" -ne 1 ]
+then
+  echo "Usage: <ca_name>"
+  exit 1
+fi
+
+CA=$1
+FOLDER=.
+openssl ocsp -index $FOLDER/db/$CA.db -port 8080 -rsigner $FOLDER/ca/$CA.ocsp.crt -rkey $FOLDER/ca/$CA.ocsp.key -CA $FOLDER/ca/$CA.crt -text -out log.txt
